5f31440f2f7c3cb20792941b8d4d2ef4fb9f1cac
crghuang <crghuang@gmail.com>
2023-09-04 18:19:49 -0600

    I3- Create facilities for all CO, NY, MO DMVs when pulled from API

--- lib/facility.rb
+++ lib/facility.rb
@@ -51,4 +51,58 @@ class Facility
     return false if !((@services.include? "Renew License") && registrant.license_data[:license])
     registrant.license_data[:renewed] = true
   end
+
+  def create_facilities(facilities)
+    facilities_list = []
+    facilities.map do |facility|
+      case facility[:state]
+      when "CO"
+        name = facility[:name]
+        address = [
+            facility[:address_li],
+            facility[:address__1],
+            facility[:city],
+            facility[:state],
+            facility[:zip]
+          ].join(" ")
+        phone = facility[:phone]
+        services = facility[:services_p].split(%r{(,|;)\s*})
+
+      when "NY"
+        name = facility[:office_name]
+        address = [
+            facility[:street_address_line_1],
+            facility[:city],
+            facility[:state],
+            facility[:zip_code]
+          ].join(" ")
+        phone = facility[:public_phone_number]
+        services = []
+
+      when "MO"
+        name = facility[:name]
+        address = [
+            facility[:address1],
+            facility[:city],
+            facility[:state],
+            facility[:zipcode]
+          ].join(" ")
+        phone = facility[:phone]
+        services = []
+      
+      else
+        name = ""
+        address = ""
+        phone = ""
+        services = []
+      end
+
+      facilities_list << Facility.new({
+        :name     => name,
+        :address  => address,
+        :phone    => phone,
+        :services => services
+      })
+    end
+  end
 end
